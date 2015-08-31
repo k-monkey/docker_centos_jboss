b50041e9a1c870b7d809100c46ac0f808cd4ccf7
k-monkey <captain.monkey88@gmail.com>
2015-08-31 14:15:49 -0700

    first version of base image ready

--- Dockerfile
+++ Dockerfile
@@ -1,43 +1,8 @@
 From centos:6
 MAINTAINER ian.wu@macys.com
 
-#install necessary packages
-RUN yum update -y
-RUN yum -y install sudo
-RUN yum -y install java-1.6.0-openjdk-devel
-RUN yum -y install wget
-RUN yum -y install unzip
-RUN yum -y install which
-
-#configure the non-root user named "dev"
-RUN useradd -ms /bin/bash dev
-RUN echo 'dev ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-# Edit sudoers file # To avoid error: sudo: sorry, you must have a tty to run sudo RUN
-RUN sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers
-
-#unpack and install jboss-6.4
+#copy all helper files to image
 ADD local/* /tmp/
-RUN unzip /tmp/jboss-eap-6.4.0.zip -d /usr/share
-RUN rm /tmp/jboss-eap-6.4.0.zip
-RUN chown -fR dev.dev /usr/share/jboss-eap-6.4/
+RUN /tmp/config.sh
 
-#configure the dev user
-USER dev
-RUN echo "export JBOSS_HOME='/usr/share/jboss-eap-6.4/'" >> ~/.bashrc
-RUN echo "alias run-jboss='/usr/share/jboss-eap-6.4/bin/standalone.sh'" >> ~/.bashrc
-
-#configure jboss
-RUN /usr/share/jboss-eap-6.4/bin/add-user.sh admin password-1
-RUN cp /tmp/order.war-1.17.17.war /usr/share/jboss-eap-6.4/standalone/deployments/order.war
-RUN cp /tmp/environment.properties /usr/share/jboss-eap-6.4/standalone/configuration/
-RUN cp /tmp/standalone.conf /usr/share/jboss-eap-6.4/bin/standalone.conf
-RUN cp /tmp/module.xml /usr/share/jboss-eap-6.4/modules/system/layers/base/sun/jdk/main/module.xml
-#ADD keystore /opt/jboss/wildfly/standalone/configuration/
-
-#change the jboss port-binding. specicially, we need to 
-#bind the ports to 0.0.0.0:xxxx , so that docker's port 
-#binding could expose the container's port to external host
-RUN sed -i -e "s/<inet-address.*\/>/<any-address\/>/g" /usr/share/jboss-eap-6.4/standalone/configuration/standalone.xml 
-
-
-
+WORKDIR /usr/share/jboss-eap-6.4/
--- local/config.sh
+++ local/config.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #install necessary packages
-#yum update -y
+#yum update -y #disabled, too many downloads
 yum -y install sudo
-yum -y install java-1.6.0-openjdk-devel
 yum -y install wget
 yum -y install unzip
 yum -y install which
+yum -y install java-1.6.0-openjdk-devel
 
 #configure the non-root user named "dev"
 useradd -ms /bin/bash dev
@@ -18,9 +18,6 @@ unzip /tmp/jboss-eap-6.4.0.zip -d /usr/share
 rm /tmp/jboss-eap-6.4.0.zip
 chown -fR dev.dev /usr/share/jboss-eap-6.4/
 
-#prepare for dev scripts to be executed
-chmod a+x /tmp/dev_commands.sh
-
 #==============
 #user commands for dev
 su dev
@@ -30,21 +27,13 @@ echo "export JAVA_OPTS='-Xmx512m -Xms256m -XX:MaxPermSize=350m -XX:PermSize=64m 
 
 #configure jboss
 /usr/share/jboss-eap-6.4/bin/add-user.sh admin password-1
-cp /tmp/order.war-1.17.17.war /usr/share/jboss-eap-6.4/standalone/deployments/order.war
-
-echo "#local settings added by ian.wu" >> /usr/share/jboss-eap-6.4/bin/standalone.conf
-echo "JAVA_OPTS=\"\$JAVA_OPTS -Dsdp.configuration.home=/usr/share/jboss-eap-6.4/standalone/configuration\"" >> /usr/share/jboss-eap-6.4/bin/standalone.conf
-cp -f /tmp/standalone.xml /usr/share/jboss-eap-6.4/standalone/configuration/
-cp -f /tmp/module.xml /usr/share/jboss-eap-6.4/modules/system/layers/base/sun/jdk/main/module.xml
-cp -f /tmp/environment.properties /usr/share/jboss-eap-6.4/standalone/configuration/
-cp -f /tmp/MA000XSPKI03.pem /usr/share/jboss-eap-6.4/bin/
-cp -f /tmp/MacysRootCA.pem /usr/share/jboss-eap-6.4/bin/
-cp -f /tmp/messserv.p12 /usr/share/jboss-eap-6.4/bin/
 
 #change the jboss port-binding. specicially, we need to 
 #bind the ports to 0.0.0.0:xxxx , so that docker's port 
 #binding could expose the container's port to external host
-sed -i -e "s/<inet-address.*\/>/<any-address\/>/g" /usr/share/jboss-eap-6.4/standalone/configuration/standalone.xml 
+#NOTE: this is not used, because we hard-coded this change in
+#a local file which will override standalone.xml later.
+#sed -i -e "s/<inet-address.*\/>/<any-address\/>/g" /usr/share/jboss-eap-6.4/standalone/configuration/standalone.xml 
 
 #enable the following  later
 #ADD keystore /opt/jboss/wildfly/standalone/configuration/
